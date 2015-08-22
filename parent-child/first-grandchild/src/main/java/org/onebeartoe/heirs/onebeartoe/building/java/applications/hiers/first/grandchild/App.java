26d4fffdccb0242368a48cd41ce05b0c77f1c639
onebeartoe <onebeartoe@gmail.com>
2015-08-22 11:26:31 -0500

    seed

---
+++ parent-child/first-grandchild/src/main/java/org/onebeartoe/heirs/onebeartoe/building/java/applications/hiers/first/grandchild/App.java
@@ -0,0 +1,13 @@
+package org.onebeartoe.heirs.onebeartoe.building.java.applications.hiers.first.grandchild;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
