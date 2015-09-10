741492a75c30db12401f1376b8d5030f95244d20
onebeartoe <onebeartoe@gmail.com>
2015-09-10 13:50:06 -0500

    A boolean member was added to keep track of toggling the two lable messages, when the button is pressed.
    Also, the lable is now updated when the button is pressed.

--- drinking-bird/src/main/java/org/onebeartoe/building/applications/drinking/bird/FXMLController.java
+++ drinking-bird/src/main/java/org/onebeartoe/building/applications/drinking/bird/FXMLController.java
@@ -7,15 +7,31 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 
-public class FXMLController implements Initializable {
-    
+public class FXMLController implements Initializable 
+{    
     @FXML
     private Label label;
     
+    private boolean lableToggle = true;
+    
     @FXML
-    private void handleButtonAction(ActionEvent event) {
+    private void handleButtonAction(ActionEvent event) 
+    {
         System.out.println("You clicked me!");
-        label.setText("Hello World!");
+        
+        String labelText;
+        
+        if(lableToggle)
+        {
+            labelText = "IT'S DRINKING THE WATER!";
+        }
+        else
+        {
+            labelText = "He's going back for more";
+        }
+        lableToggle = !lableToggle;
+        
+        label.setText(labelText);
     }
     
     @Override
