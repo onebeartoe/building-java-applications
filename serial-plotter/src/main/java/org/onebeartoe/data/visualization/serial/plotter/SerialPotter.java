c058a376a00cdd3b5c1ad657ef98ffaf690a3875
onebeartoe <onebeartoe@gmail.com>
2015-12-08 12:50:43 -0600

    The data is now dynamicly put into a map based on the name of the data channel.

--- serial-plotter/src/main/java/org/onebeartoe/data/visualization/serial/plotter/SerialPotter.java
+++ serial-plotter/src/main/java/org/onebeartoe/data/visualization/serial/plotter/SerialPotter.java
@@ -12,6 +12,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -49,18 +50,18 @@ public class SerialPotter extends Application implements SerialPortEventListener
 {
     private Logger logger;
     
-    private NumberAxis xAxis;
+    private NumberAxis xAxis = new NumberAxis(0,MAX_DATA_POINTS,MAX_DATA_POINTS/10);
     
     private static final int MAX_DATA_POINTS = 50;
     
-    private XYChart.Series internalTemperatureSeries;
-    private ConcurrentLinkedQueue<Number> internalTemperatureMessageQueue = new ConcurrentLinkedQueue();
-    
-    private XYChart.Series internalHumiditySeries;
-    private ConcurrentLinkedQueue<Number> internalHumidityMessageQueue = new ConcurrentLinkedQueue();
-    
-    private XYChart.Series externalTemperatureSeries;
-    private ConcurrentLinkedQueue<Number> externalTemperatureMessageQueue = new ConcurrentLinkedQueue();
+//    private XYChart.Series internalTemperatureSeries;
+//    private ConcurrentLinkedQueue<Number> internalTemperatureMessageQueue = new ConcurrentLinkedQueue();
+//    
+//    private XYChart.Series internalHumiditySeries;
+//    private ConcurrentLinkedQueue<Number> internalHumidityMessageQueue = new ConcurrentLinkedQueue();
+//    
+//    private XYChart.Series externalTemperatureSeries;
+//    private ConcurrentLinkedQueue<Number> externalTemperatureMessageQueue = new ConcurrentLinkedQueue();
     
     private int xSeriesData = 0;
     
@@ -68,50 +69,95 @@ public class SerialPotter extends Application implements SerialPortEventListener
     
     private FXMLLoader loader;
     
-    private Map<String, XYChart.Series> dataMap;
+    private Map<String, DataChannel> dataMap;
     
     private final String DATA_PROVIDER_KEY = "dataProvider";
     
 private volatile List<String> messages;
 
     private BufferedReader input;
+
+    NumberAxis yAxis = new NumberAxis();
     
     private SerialPort serialPort;
     
-    private void addDataToSeries()
+    /**
+     * This is the chart that is presented to the user.
+     */
+    final AreaChart<Number, Number> sc = new AreaChart<Number, Number>(xAxis, yAxis) 
     {
-        if( !internalTemperatureMessageQueue.isEmpty() )
+        @Override 
+        protected void dataItemAdded(XYChart.Series<Number, Number> series, int itemIndex, XYChart.Data<Number, Number> item) 
         {
-            AreaChart.Data data = new AreaChart.Data(xSeriesData++, internalTemperatureMessageQueue.remove());
-            internalTemperatureSeries.getData().add(data);
-        }        
-        // remove points to keep us at no more than MAX_DATA_POINTS
-        if(internalTemperatureSeries.getData().size() > MAX_DATA_POINTS) 
-        {
-            internalTemperatureSeries.getData().remove(0, internalTemperatureSeries.getData().size() - MAX_DATA_POINTS);
+            ; // This method is overriden with empty statement, to remove symbols on each data point.
         }
+    };    
+    
+    private synchronized void addDataToSeries(String dataName)
+    {
+//        System.out.println("adding data to series");
         
-        if( !internalHumidityMessageQueue.isEmpty() )
-        {
-            AreaChart.Data data = new AreaChart.Data(xSeriesData-1, internalHumidityMessageQueue.remove() );
-            internalHumiditySeries.getData().add(data);
-        }
-        if(internalHumiditySeries.getData().size() > MAX_DATA_POINTS)
-        {
-            internalHumiditySeries.getData().remove(0, internalHumiditySeries.getData().size() - MAX_DATA_POINTS);
-        }
+        Set<String> keySet = dataMap.keySet();
         
-        if( !externalTemperatureMessageQueue.isEmpty() )
-        {
-            AreaChart.Data data = new AreaChart.Data(xSeriesData-1, externalTemperatureMessageQueue.remove() );
-            externalTemperatureSeries.getData().add(data);
-        }
-        if(externalTemperatureSeries.getData().size() > MAX_DATA_POINTS)
-        {
-            int size = externalTemperatureSeries.getData().size() - MAX_DATA_POINTS;
-            ObservableList data = externalTemperatureSeries.getData();
-            data.remove(0, size);
-        }
+//        keySet.stream()
+//                .forEach( k -> 
+//                {
+                    DataChannel dc = dataMap.get(dataName);
+//                    DataChannel dc = dataMap.get(k);
+                    ConcurrentLinkedQueue<Number> dataQueue = dc.getDataQueue();
+                    if( !dataQueue.isEmpty() )
+                    {
+                        int xPosition = dc.getxPosition();
+                        Number value = dataQueue.remove();
+                        AreaChart.Data acData = new AreaChart.Data(xSeriesData, value);
+                        XYChart.Series cs = dc.getChartSeries();
+                        cs.getData().add(acData);
+                        
+                        dc.setxPosition(xPosition+1);
+                     
+                        if(cs.getData().size() > MAX_DATA_POINTS)
+                        {
+                            int size = cs.getData().size() - MAX_DATA_POINTS;
+                            ObservableList data = cs.getData();
+                            data.remove(0, size);
+                        }
+                    }
+//                });
+        
+        xSeriesData++;
+
+//        if( !internalTemperatureMessageQueue.isEmpty() )
+//        {
+//            AreaChart.Data data = new AreaChart.Data(xSeriesData++, internalTemperatureMessageQueue.remove());
+//            internalTemperatureSeries.getData().add(data);
+//        }        
+//        // remove points to keep us at no more than MAX_DATA_POINTS
+//        if(internalTemperatureSeries.getData().size() > MAX_DATA_POINTS) 
+//        {
+//            internalTemperatureSeries.getData().remove(0, internalTemperatureSeries.getData().size() - MAX_DATA_POINTS);
+//        }
+//        
+//        if( !internalHumidityMessageQueue.isEmpty() )
+//        {
+//            AreaChart.Data data = new AreaChart.Data(xSeriesData-1, internalHumidityMessageQueue.remove() );
+//            internalHumiditySeries.getData().add(data);
+//        }
+//        if(internalHumiditySeries.getData().size() > MAX_DATA_POINTS)
+//        {
+//            internalHumiditySeries.getData().remove(0, internalHumiditySeries.getData().size() - MAX_DATA_POINTS);
+//        }
+//        
+//        if( !externalTemperatureMessageQueue.isEmpty() )
+//        {
+//            AreaChart.Data data = new AreaChart.Data(xSeriesData-1, externalTemperatureMessageQueue.remove() );
+//            externalTemperatureSeries.getData().add(data);
+//        }
+//        if(externalTemperatureSeries.getData().size() > MAX_DATA_POINTS)
+//        {
+//            int size = externalTemperatureSeries.getData().size() - MAX_DATA_POINTS;
+//            ObservableList data = externalTemperatureSeries.getData();
+//            data.remove(0, size);
+//        }
                 
         // update 
         xAxis.setLowerBound(xSeriesData-MAX_DATA_POINTS);
@@ -120,7 +166,7 @@ private volatile List<String> messages;
     
     @Override
     /**
-     * 
+     * This is called to initialize the JavaFX application.
      */
     public void init()
     {
@@ -129,6 +175,8 @@ private volatile List<String> messages;
         dataMap = new HashMap();
         
         messages = new ArrayList();
+        
+//        sc.set
         
         initializeSerialPort();
     }    
@@ -150,7 +198,7 @@ private volatile List<String> messages;
         }
         catch (Exception | NoClassDefFoundError ex)
         {
-            logger.log(Level.SEVERE, null, ex);
+//            logger.log(Level.SEVERE, null, ex);
         }
     }
 
@@ -167,22 +215,22 @@ private volatile List<String> messages;
         launch(args);
     }
     
-    /**
-     * Timeline gets called in the JavaFX Main thread
-     */ 
-    private void prepareTimeline() 
-    {        
-        // Every frame to take any data from queue and add to chart
-        new AnimationTimer() 
-        {
-            // is this the proper Thread to kill in primaryStage.setOnCloseRequest() ????
-            @Override 
-            public void handle(long now) 
-            {
-                addDataToSeries();
-            }
-        }.start();
-    }    
+//    /**
+//     * Timeline gets called in the JavaFX Main thread
+//     */ 
+//    private void prepareTimeline() 
+//    {        
+//        // Every frame to take any data from queue and add to chart
+//        new AnimationTimer() 
+//        {
+//            // is this the proper Thread to kill in primaryStage.setOnCloseRequest() ????
+//            @Override 
+//            public void handle(long now) 
+//            {
+//                addDataToSeries();
+//            }
+//        }.start();
+//    }    
 
     /**
      * Handle an event on the serial port. Read the data and print it.
@@ -202,43 +250,46 @@ private volatile List<String> messages;
                 
                 if( split.length >= 2)
                 {
-                    String dataName = split[0];
-
-                    String s = split[1];                    
-                    double d  = Double.valueOf(s);
-
-                    switch(dataName)
+                    String dataName = split[0].trim();
+                    
+                    if( dataName.length() != 0)
                     {
-                        case "S":
-                        {
-                            internalTemperatureMessageQueue.add(d);
-                            System.out.println(inputLine);
+                        String s = split[1];                    
+                        double d  = Double.valueOf(s);
 
-                            break;
-                        }
-                        case "B":
+                        DataChannel dataChannel = dataMap.get(dataName);
+                        if(dataChannel == null)
                         {
-                            externalTemperatureMessageQueue.add(d);
-                            System.out.println(inputLine);
+                            dataChannel = new DataChannel();
 
-                            break;
+                            XYChart.Series chartSeries = dataChannel.getChartSeries();
+                            chartSeries.setName(dataName);                                
+
+                            Platform.runLater(
+                                    () -> {sc.getData().add(chartSeries);}
+                            );
+
+
+                            dataMap.put(dataName, dataChannel);
                         }
-                        case "Q":
+
+                        dataChannel.getDataQueue().add(d);                    
+
+                        int size = messages.size();
+                        if(size >= 50)
                         {
-                            internalHumidityMessageQueue.add(d);
-                            System.out.println(inputLine);
-
-                            break;
+                            messages.remove(0);
                         }
+
+                        Platform.runLater(
+                            () -> 
+                            {
+                                addDataToSeries(dataName);
+                            }
+                        );
+
+                        System.out.println(inputLine + ": " + xSeriesData);
                     }
-
-                    int size = messages.size();
-                    if(size >= 50)
-                    {
-                        messages.remove(0);
-                    }
-
-                    System.out.println(inputLine);                    
                 }
                 else
                 {
@@ -247,7 +298,7 @@ private volatile List<String> messages;
             } 
             catch (Exception e) 
             {
-                System.err.println(e.toString());
+//                System.err.println(e.toString());
             }
         }
         else
@@ -269,24 +320,13 @@ private volatile List<String> messages;
         scene.getStylesheets().add("/styles/Styles.css");
 
         logger = Logger.getLogger(getClass().getName());
-        
-        xAxis = new NumberAxis(0,MAX_DATA_POINTS,MAX_DATA_POINTS/10);
+                
         xAxis.setForceZeroInRange(false);
         xAxis.setAutoRanging(false);
         
-        NumberAxis yAxis = new NumberAxis();
+//        NumberAxis yAxis = new NumberAxis();
         yAxis.setAutoRanging(true);
-        
-        
-        // Chart
-        final AreaChart<Number, Number> sc = new AreaChart<Number, Number>(xAxis, yAxis) 
-        {
-            @Override 
-            protected void dataItemAdded(XYChart.Series<Number, Number> series, int itemIndex, XYChart.Data<Number, Number> item) 
-            {
-                ; // This method is overriden with empty statement, to remove symbols on each data point.
-            }
-        };
+
         sc.setAnimated(false);
         sc.setId("liveAreaChart");
         sc.setTitle("Lizard Enclosure Sensor Readings");
@@ -309,18 +349,18 @@ private volatile List<String> messages;
         
         children.add(sc);
 
-        // internal temperature Series
-        internalTemperatureSeries = new AreaChart.Series<Number, Number>();
-        internalTemperatureSeries.setName("Internal Temperature");
-        sc.getData().add(internalTemperatureSeries);        
-        
-        externalTemperatureSeries = new AreaChart.Series<Number, Number>();
-        externalTemperatureSeries.setName("External Temperature");
-        sc.getData().add(externalTemperatureSeries);
-        
-        internalHumiditySeries = new AreaChart.Series<Number, Number>();
-        internalHumiditySeries.setName("Internal Humidity");
-        sc.getData().add(internalHumiditySeries);
+//        // internal temperature Series
+//        internalTemperatureSeries = new AreaChart.Series<Number, Number>();
+//        internalTemperatureSeries.setName("Internal Temperature");
+//        sc.getData().add(internalTemperatureSeries);        
+//        
+//        externalTemperatureSeries = new AreaChart.Series<Number, Number>();
+//        externalTemperatureSeries.setName("External Temperature");
+//        sc.getData().add(externalTemperatureSeries);
+//        
+//        internalHumiditySeries = new AreaChart.Series<Number, Number>();
+//        internalHumiditySeries.setName("Internal Humidity");
+//        sc.getData().add(internalHumiditySeries);
 
         stage.setScene(scene);
         
@@ -338,7 +378,7 @@ private volatile List<String> messages;
         stage.setScene(scene);
         stage.show();
         
-        prepareTimeline();        
+//        prepareTimeline();        
     }
     
     @Override
@@ -348,6 +388,52 @@ private volatile List<String> messages;
         {
             serialPort.removeEventListener();
             serialPort.close();
+        }        
+    }
+    
+    private class DataChannel
+    {
+        private int xPosition;
+        
+        private XYChart.Series chartSeries;
+        
+        private ConcurrentLinkedQueue<Number> dataQueue;
+        
+        public DataChannel()
+        {
+            xPosition = 0;
+            chartSeries = new AreaChart.Series<Number, Number>();
+            dataQueue = new ConcurrentLinkedQueue();
+        }
+
+        public int getxPosition()
+        {
+            return xPosition;
+        }
+
+        public void setxPosition(int xPosition)
+        {
+            this.xPosition = xPosition;
+        }
+
+        public XYChart.Series getChartSeries()
+        {
+            return chartSeries;
+        }
+
+        public void setChartSeries(XYChart.Series chartSeries)
+        {
+            this.chartSeries = chartSeries;
+        }
+
+        public ConcurrentLinkedQueue<Number> getDataQueue()
+        {
+            return dataQueue;
+        }
+
+        public void setDataQueue(ConcurrentLinkedQueue<Number> dataQueue)
+        {
+            this.dataQueue = dataQueue;
         }        
     }
 }
