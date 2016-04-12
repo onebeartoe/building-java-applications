4c4a2ba01eec61fc498429940d26e56b45a76633
onebeartoe <onebeartoe@gmail.com>
2016-04-11 23:16:31 -0500

    This is the seed of the microcontroller code for this project.

---
+++ continuous-integration/extreme-notifications/src/main/arduino/jinkens_rss_notifier/jinkens_rss_notifier.ino
@@ -0,0 +1,35 @@
+
+char inData[64];
+char inChar=-1;
+
+void setup() 
+{
+   Serial.begin(9600);
+   Serial.println("Waiting for Raspberry Pi to send a signal...\n");
+}
+
+void loop() 
+{
+  // put your main code here, to run repeatedly:
+    byte numBytesAvailable= Serial.available();
+
+    // if there is something to read
+    if (numBytesAvailable > 0){
+        // store everything into "inData"
+
+// doh!
+        Serial.readLine();
+        
+        int i;
+        for (i=0;i<numBytesAvailable;i++){
+            inChar= Serial.read();
+            inData[i] = inChar;
+        }
+
+        inData[i] = '\0';
+
+
+        Serial.print("Arduino Received: ");
+        Serial.println(inData);
+    }
+}
