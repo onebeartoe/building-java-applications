779c2a8d60b7718cc97b3dae97fddfccf309e879
onebeartoe <onebeartoe@gmail.com>
2016-04-15 14:13:32 -0500

    Very basic communication is working.  The Arduino reads a line from the serial input and prints with it received.

--- continuous-integration/extreme-notifications/src/main/arduino/jinkens_rss_notifier/jinkens_rss_notifier.ino
+++ continuous-integration/extreme-notifications/src/main/arduino/jinkens_rss_notifier/jinkens_rss_notifier.ino
@@ -5,31 +5,19 @@ char inChar=-1;
 void setup() 
 {
    Serial.begin(9600);
-   Serial.println("Waiting for Raspberry Pi to send a signal...\n");
+   Serial.println("Hello Jenkins RSS notifier world!\n");
 }
 
 void loop() 
 {
-  // put your main code here, to run repeatedly:
     byte numBytesAvailable= Serial.available();
 
     // if there is something to read
-    if (numBytesAvailable > 0){
-        // store everything into "inData"
-
-// doh!
-        Serial.readLine();
-        
-        int i;
-        for (i=0;i<numBytesAvailable;i++){
-            inChar= Serial.read();
-            inData[i] = inChar;
-        }
-
-        inData[i] = '\0';
-
-
+    if (numBytesAvailable > 0)
+    {    
+        String s = Serial.readString();
+ 
         Serial.print("Arduino Received: ");
-        Serial.println(inData);
+        Serial.println(s);
     }
 }
