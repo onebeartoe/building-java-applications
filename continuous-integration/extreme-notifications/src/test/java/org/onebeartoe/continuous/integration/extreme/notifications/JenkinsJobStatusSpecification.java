a0bb4fd2400503783a095b4ac8d3c7aac80b290f
Roberto Marquez <onebeartoe@gmail.com>
2018-12-10 15:32:24 -0600

    More tests were added.

---
+++ continuous-integration/extreme-notifications/src/test/java/org/onebeartoe/continuous/integration/extreme/notifications/JenkinsJobStatusSpecification.java
@@ -0,0 +1,28 @@
+/*
+ */
+package org.onebeartoe.continuous.integration.extreme.notifications;
+
+import java.awt.Color;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author Roberto Marquez
+ */
+public class JenkinsJobStatusSpecification
+{
+    private JenkinsJobStatus implementation;
+    
+    public JenkinsJobStatusSpecification()
+    {
+        implementation = JenkinsJobStatus.STABLE;
+    }
+    
+    @Test(groups = {"unit"})
+    public void getColor()
+    {
+        Color actual = implementation.getColor();
+
+        assert( Color.green.getRGB() == actual.getRGB() );
+    }
+}
