aa2fe0d9c4ea9098fbf0f92e0f642a57b3f64f47
Roberto Marquez <onebeartoe@gmail.com>
2018-12-13 08:21:20 -0600

    The code was cleaned up a little bit.

--- web/automation/src/test/java/org/onebeartoe/web/automation/UnitsOfWork.java
+++ web/automation/src/test/java/org/onebeartoe/web/automation/UnitsOfWork.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.remote.RemoteWebDriver;
  * 
  * The UnitsOfWork class' operations navigate between Page objects.
  * 
- * Some setup configuration was inspired by this blog entry:
+ * Some setup configuration was partially inspired by this blog entry:
  * 
  *      https://www.hindsightsoftware.com/blog/selenium-webdriver-java
  * 
