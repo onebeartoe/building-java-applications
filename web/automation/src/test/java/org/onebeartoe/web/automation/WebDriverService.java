c18b55712e042b6f00ab5b444398564798d35d8a
Roberto Marquez <onebeartoe@gmail.com>
2018-12-13 01:29:28 -0600

    Headless mode was hardcoded in for Chrome.

--- web/automation/src/test/java/org/onebeartoe/web/automation/WebDriverService.java
+++ web/automation/src/test/java/org/onebeartoe/web/automation/WebDriverService.java
@@ -6,19 +6,18 @@ import java.net.MalformedURLException;
 
 import org.apache.commons.configuration.ConfigurationException;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
-//TODO: Is the ready for deprecation?
-//TODO: See https://github.com/paulakimenko/webdriver-service/
 public class WebDriverService
 {
     public RemoteWebDriver load(WebDriverType type) throws ConfigurationException, MalformedURLException
     {
         DesiredCapabilities capabilities = new DesiredCapabilities();
-//        capabilities.setc
+
         RemoteWebDriver driver;
 
         //TODO find a better way to choose the driver implementation
@@ -29,9 +28,10 @@ public class WebDriverService
                 String driverPath = "/opt/google/chromedriver/chromedriver-2.37/chromedriver";
                 System.setProperty("webdriver.chrome.driver", driverPath);
                 
-// This was the original call that we know works.
-//                driver = new ChromeDriver();                
-                driver = new ChromeDriver(capabilities);
+                ChromeOptions options = new ChromeOptions();
+                options.setHeadless(true);
+                
+                driver = new ChromeDriver(options);
 
                 break;
             }
@@ -53,17 +53,17 @@ public class WebDriverService
         return driver; 
     }
 
-	/**
-	 * This has the webdriver.tests through Firefox browser.
-	 * 
-	 * @return
-	 */
-	private RemoteWebDriver loadFirefox(DesiredCapabilities capabilities)
-	{
-            FirefoxProfile profile = new FirefoxProfile();
-            capabilities.setCapability(FirefoxDriver.PROFILE, profile);
-            RemoteWebDriver driver = new FirefoxDriver(capabilities);
+    /**
+     * This has the webdriver.tests through Firefox browser.
+     * 
+     * @return
+     */
+    private RemoteWebDriver loadFirefox(DesiredCapabilities capabilities)
+    {
+        FirefoxProfile profile = new FirefoxProfile();
+        capabilities.setCapability(FirefoxDriver.PROFILE, profile);
+        RemoteWebDriver driver = new FirefoxDriver(capabilities);
 
-            return driver;
-	}
+        return driver;
+    }
 }
