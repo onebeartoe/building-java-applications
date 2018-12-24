315042fd63f3fe32e6cf158e6a2e88803904421e
Roberto Marquez <onebeartoe@gmail.com>
2018-12-24 10:51:27 -0600

    More unit tests were added.

---
+++ continuous-integration/extreme-notifications/src/test/java/org/onebeartoe/continuous/integration/extreme/notifications/ArduinoListenerSpecification.java
@@ -0,0 +1,11 @@
+
+package org.onebeartoe.continuous.integration.extreme.notifications;
+
+/**
+ *
+ * @author Roberto Marquez
+ */
+public class ArduinoListenerSpecification
+{
+// add some tests    
+}
